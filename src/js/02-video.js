c4483cd2c75e402d7f858690ed886a9b0c0a3b20
OleksiiChernii <oleksii.chernii@gmail.com>
2022-10-16 15:54:38 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const videoplayerCurrentTime = 'videoplayer-current-time';
+const _ = require('lodash');
+const saveCurrnetTime = _.throttle((seconds) => localStorage.setItem(videoplayerCurrentTime, seconds), 1000);
+
+let currentTime = localStorage.getItem(videoplayerCurrentTime);
+
+if (currentTime) {
+  player.setCurrentTime(parseFloat(currentTime));
+}
+
+player.on('timeupdate', ({seconds}) => saveCurrnetTime(seconds));
 
