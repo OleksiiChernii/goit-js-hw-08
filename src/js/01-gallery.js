9bb364fb731423466ccf768b58b58c73875bfee3
OleksiiChernii <oleksii.chernii@gmail.com>
2022-10-17 21:41:07 +0300

    fixed task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,31 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
-gallery.append(...insertImages(galleryItems));
+gallery.append(...galleryItems.map(createLink));
 
 gallery.addEventListener('click', event => event.preventDefault);
 
-function insertImages(elements) {
-  return elements.map(element => createLink(element).appendChild(createImage(element)));
+function createLink(element) {
+  let link = document.createElement('a');
+  link.classList.add('gallery__item');
+  link.setAttribute('href', element.original);
+  link.append(createImage(element));
+  return link;
 }
 
-function createLink({original}){
-    let link = document.createElement('a')
-    link.classList.add('gallery__item');
-    link.setAttribute('href', original);
-    return link;
-}
-
-function createImage({preview, description}){
-    let img = document.createElement('img');
-    img.classList.add('gallery__image');
-    img.setAttribute('src', preview);
-    img.setAttribute('alt', description);
-    return img;
+function createImage({ preview, description }) {
+  let img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.setAttribute('src', preview);
+  img.setAttribute('alt', description);
+  return img;
 }
 
 new SimpleLightbox('.gallery a', { docClose: true, captionsData: 'alt', captionDelay: 250});
